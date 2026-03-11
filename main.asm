8bc8a2578f6e1b09c58f5097a7a0a37d462c68a9
r006 <rajeshsharma06@yahoo.com>
2026-03-11 17:43:17 -0400

    Updated UART and TACH

--- main.asm
+++ main.asm
@@ -1,3 +1,4 @@
+;WORKING MAIN
 LIST    p=PIC16F18877		
     #INCLUDE <p16f18877.inc>
     
@@ -12,7 +13,7 @@ LIST    p=PIC16F18877
     ; CONFIG5
     __CONFIG _CONFIG5, _CP_OFF & _CPD_OFF
     
-
+; Common
 ADC_4BIT       EQU 0x70
 ADC_H8BIT      EQU 0x71   
 ADC_L2BIT      EQU 0x72    
@@ -22,25 +23,24 @@ DELAY_S	       EQU 0x75
 DELAY_MS       EQU 0x76
 DELAY_US       EQU 0x77
 TIMER_OVERFLOW_COUNTER EQU 0x78
-TIMER_TOGGLE   EQU 0x79
-MODE           EQU 0x7A
+TIMER_TOGGLE_BUZZER    EQU 0x79
+TIMER_TOGGLE_UART      EQU 0x7A    
+MODE           EQU 0x7B
 
-TACH_PERIODL   EQU 0x20    
-TACH_PERIODH   EQU 0x21    
-TACH_PREVL     EQU 0x22    
-TACH_PREVH     EQU 0x23    
-TACH_RPML      EQU 0x24    
-TACH_RPMH      EQU 0x25    
+;Bank 0	 (Uart and Tach)  
+PULSE_L	       EQU 0x20    
+PULSE_H	       EQU 0x21    
+TACH_RPML      EQU 0x22    
+TACH_RPMH      EQU 0x23
+TACH_toBCDL    EQU 0x24
+TACH_toBCDH    EQU 0x25
 
 DIGIT_1000     EQU 0x26    
 DIGIT_100      EQU 0x27    
 DIGIT_10       EQU 0x28    
-DIGIT_1        EQU 0x29	   
-	   
-	   
-
-   
-   
+DIGIT_1        EQU 0x29
+	
+LOOP_COUNT     EQU 0x2A
 	   
  
     ORG 0x00
@@ -54,7 +54,9 @@ DIGIT_1        EQU 0x29
     #INCLUDE <fanspeed.inc>
     #INCLUDE <mode.inc>
     #INCLUDE <seven_seg_disp.inc>
+    #INCLUDE <tach.inc>
     #INCLUDE <timer.inc>
+    #INCLUDE <uart.inc>
 	
 
 begin
@@ -63,6 +65,8 @@ begin
     CALL    init_buzzer_PWM
     CALL    init_timer1
     CALL    init_fanspeed_PWM
+    CALL    init_tach
+    CALL    init_uart
     
     BANKSEL MODE
     CLRF    MODE            ; Default to DIP Switch mode (0)
@@ -70,8 +74,15 @@ begin
     
     BANKSEL LATB
     BCF     LATB, 0         ; Status LED ON (Active-Low for DIP Mode)
+    
+    CALL    uart_test       ;Test UART Ouptut "Measuring Fan Speed" 
 
+    
+    
+    
+    
 MAIN_LOOP
+
     CALL    check_reset	    ; emergency reset
     CALL    adc_convert     ; POT to 10-bit (Left justified to 8-bit)
     CALL    adc_4bit        ; 8-bit ADRESH to 4-bit
@@ -79,9 +90,12 @@ MAIN_LOOP
     CALL    press_button    ; Check RE1 for mode toggle
     CALL    mode_select     ; Select between ADC_4BIT and DIP_4BIT
     CALL    disp_seven_seg  ; Update 7-segment display
-    CALL    timer1_run	    ; TIMER_TOGGLE from 0 to 1 every second
+    CALL    timer1_run	    ; TIMER_TOGGLE (BUZZER and UART) from 0 to 1 every second
     CALL    check_alarmState; Check if ADC_4BIT >= 12
     CALL    adjust_fanspeed ; ADRESH to CCPR2H, ADRESL to CCPR2L to modify pulse width
+    CALL    get_fan_speed   ; Count pulses in 1 second (RPS) and convert to RPM 
+    CALL    uart_send_rpm   ; Convert RPM to BCD, Sends "RPM=XXXX\r\n" and clears TIMER_TOGGLE_UART
+    
     GOTO    MAIN_LOOP       
     
     END
